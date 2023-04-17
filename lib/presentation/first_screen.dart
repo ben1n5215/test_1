d1140e825e05b79e4d03ea1da9c6bd7e29e066c8
Benik <galstyan.benik@gmail.com>
2023-04-17 22:00:52 +0300

    implemented first screen with navigation to the second screen

---
+++ lib/presentation/first_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:test_1/presentation/second_screen.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("button"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
